6987bd2dccf60823a65cf30ec215e8d20c4cb054
dominayo <victor.nelson.2321@gmail.com>
2022-06-25 18:08:07 -0700

    added comments

--- substradix/src/rng.rs
+++ substradix/src/rng.rs
@@ -1,5 +1,8 @@
 use scrypto::prelude::*;
 
+// Runtime::generate_uuid() is not a verifiably random number, but for the purposes of this example, assume it is.
+// A final implementation of this blueprint will use an oracle to ensure numbers truely are random.
+
 pub fn seed(min: u128, max: u128) -> u128 {
     let magnitude: u128 = max-min;
     let pseudorandom_number: u128 = Runtime::generate_uuid();
