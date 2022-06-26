59e1e826cb9e2c7aebad9a089bd6c5dddeed0bcc
dominayo <victor.nelson.2321@gmail.com>
2022-06-25 17:10:04 -0700

    Added Comments, cleaned logic/info boxes

--- substradix/src/combat.rs
+++ substradix/src/combat.rs
@@ -2,32 +2,33 @@ use scrypto::prelude::*;
 use super::structs;
 use super::rng;
 
-// Calculates the combat between user and enemy. Enemy Data + rewards set by caller. Returns health after combat
-pub fn combat( mut player_stats: structs::CombatInfo, mut enemy_stats: structs::CombatInfo ) -> Decimal {
-    let mut rounds: u128 = 0;
-    let mut hits: u128 = 0;
-    loop {
+// Calculates the combat between player and enemy. Returns player health
+pub fn combat(mut player_stats: structs::CombatInfo, mut enemy_stats: structs::CombatInfo ) -> Decimal {
+    let mut rounds: u16 = 0;
+    let mut hits: u16 = 0;
+    let mut enemy_hits: u16 = 0;
+    // Loop which runs until one of the fighter's health is 0
+    'outer: loop {
         rounds += 1;
-        info!("Round {}:", rounds);
+        info!("Turn {}:", rounds);
         let damage_given = std::cmp::max(Decimal::one(), player_stats.damage - (enemy_stats.defense / dec!(2)));
         let damage_taken = std::cmp::max(Decimal::one(), enemy_stats.damage - (player_stats.defense / dec!(2)));
         let mut priority = player_stats.speed / enemy_stats.speed * rng::seed_decimal(75,125,dec!(100));
-        info!("Damage Given: {}", damage_given);
-        info!("Damage taken: {}", damage_taken);
-        info!("Prio: {}", priority);
+        // Check if a character has died.
         if player_stats.health <= dec!(0) {
             info!("PlayerDies");
             return player_stats.health
-            
         };
         if enemy_stats.health <= dec!(0) {
             info!("EnemyDies");
             return player_stats.health
         };
         if priority >= dec!(1) {
+            info!("Player attacks");
+            // Loop which implements the amount of hits the player can do, depending on priority. 
             loop {
-                info!("Player attacks");
-                enemy_stats.health -= Decimal::round(&(damage_given * rng::seed_decimal(75,125,dec!(100))), 0, RoundingMode::TowardsNearestAndHalfTowardsZero);
+                // Applies random variance to the base damage, and rounds that damage nummber to the nearest integer
+                enemy_stats.health -= Decimal::round(&(damage_given * rng::seed_decimal(75,125,dec!(100))), 0, RoundingMode::TowardsNearestAndHalfTowardsZero); 
                 hits += 1;
                 info!("Hits: {}", hits);
                 info!("Health: {} Enemy Health: {}", player_stats.health, enemy_stats.health);
@@ -39,34 +40,37 @@ pub fn combat( mut player_stats: structs::CombatInfo, mut enemy_stats: structs::
                     info!("EnemyDies");
                     return player_stats.health
                 };
+                // Changes priority to limit the amount of attacks per turn
                 priority *= dec!(".9");
                 if priority >= dec!(1) {
                     continue;
                 }
+                // Once priority is below 1, the enemy gets a hit and the round is over.
                 else {
                     info!("Enemy attacks");
                     player_stats.health -= Decimal::round(&(damage_taken * rng::seed_decimal(75,125,dec!(100))), 0, RoundingMode::TowardsNearestAndHalfTowardsZero);
-                    hits = 0;
+                    enemy_hits = 1;
+                    info!("Enemy hits: {}", enemy_hits);
                     info!("Health: {} Enemy Health: {}", player_stats.health,enemy_stats.health);
-                        if player_stats.health < dec!(0) || player_stats.health == dec!(0) {
-                            info!("PlayerDies");
-                            return player_stats.health
-                        }
-                        if enemy_stats.health < dec!(0) || enemy_stats.health == dec!(0) {
-                            info!("EnemyDies");
-                            return player_stats.health
-                        }
-                        break;
+                    if player_stats.health < dec!(0) || player_stats.health == dec!(0) {
+                        info!("PlayerDies");
+                        return player_stats.health
+                    }
+                    if enemy_stats.health < dec!(0) || enemy_stats.health == dec!(0) {
+                        info!("EnemyDies");
+                        return player_stats.health
+                    }
+                    continue 'outer;
                 }
             }
-        };
-        if priority < dec!(1) {
+        }
+        else {
             info!("Enemy attacks");
-            hits = 0;
+            // Loop which implements the amount of hits the enemy can do
             loop {
                 player_stats.health -= Decimal::round(&(damage_taken * rng::seed_decimal(75,125,dec!(100))), 0, RoundingMode::TowardsNearestAndHalfTowardsZero);
-                hits += 1;
-                info!("Enemy hits: {}", hits);
+                enemy_hits += 1;
+                info!("Enemy hits: {}", enemy_hits);
                 info!("Health: {} Enemy Health: {}", player_stats.health,enemy_stats.health);
                 if player_stats.health <= dec!(0) {
                     info!("PlayerDies");
@@ -76,7 +80,7 @@ pub fn combat( mut player_stats: structs::CombatInfo, mut enemy_stats: structs::
                     info!("EnemyDies");
                     return player_stats.health
                 }
-                priority *= dec!("1.1");
+                priority *= dec!("1.15");
                 if priority < dec!(1) {
                 continue;
                 }
@@ -86,13 +90,15 @@ pub fn combat( mut player_stats: structs::CombatInfo, mut enemy_stats: structs::
                     hits = 1;
                     info!("Hits: {}", hits);
                     info!("Health: {} Enemy Health: {}", player_stats.health,enemy_stats.health);
-                    if player_stats.health < dec!(0) || player_stats.health == dec!(0) {
+                    if player_stats.health <= dec!(0){
+                        info!("PlayerDies");
                         return player_stats.health
                     }
-                    if enemy_stats.health < dec!(0) || enemy_stats.health == dec!(0) {
+                    if enemy_stats.health <= dec!(0) {
+                        info!("EnemyDies");
                         return player_stats.health
                     }
-                    break;
+                    continue 'outer;
                 }
             }
         };
